82e502c7a0cf5b50871edf966ce8a22c72529d5d
Nick Gaudreau <nickgaudreau@outlook.com>
2022-07-20 20:19:28 -0400

    Identity App (Cookie Single Sign On API ) - cookie auth program config

--- API.SingleSignOnCookie/Program.cs
+++ API.SingleSignOnCookie/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Auth
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+   {
+       options.ExpireTimeSpan = TimeSpan.FromHours(24);
+       options.SlidingExpiration = true;
+       options.AccessDeniedPath = "/Auth/unauthorized";
+   });
 
 var app = builder.Build();
 
@@ -18,7 +30,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+var cookiePolicyOptions = new CookiePolicyOptions
+{
+    MinimumSameSitePolicy = SameSiteMode.Lax, // TODO: test with Strict
+};
+app.UseCookiePolicy(cookiePolicyOptions);
 
 app.MapControllers();
 
