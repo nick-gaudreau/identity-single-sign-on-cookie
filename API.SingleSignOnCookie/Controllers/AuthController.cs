a2968e87922f6af9ff6a71871aad879ed35f0442
Nick Gaudreau <nickgaudreau@outlook.com>
2022-07-21 21:51:03 -0400

    Auth contrl, Login endpoint with set-cookie response

---
+++ API.SingleSignOnCookie/Controllers/AuthController.cs
@@ -0,0 +1,93 @@
+using API.SingleSignOnCookie.Controllers.Models;
+using API.SingleSignOnCookie.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace API.SingleSignOnCookie.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            if (string.IsNullOrWhiteSpace(loginDto.Email) ||
+                string.IsNullOrWhiteSpace(loginDto.Password)) return Forbid();
+
+            var user = MockAuthenticateUser(loginDto);
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()),
+                //new Claim(ClaimTypes.Role, "Administrator"),
+            };
+
+            var claimsIdentity = new ClaimsIdentity(
+                claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var authProperties = new AuthenticationProperties
+            {
+                //AllowRefresh = <bool>,
+                // Refreshing the authentication session should be allowed.
+
+                //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                // The time at which the authentication ticket expires. A
+                // value set here overrides the ExpireTimeSpan option of
+                // CookieAuthenticationOptions set with AddCookie.
+
+                IsPersistent = true,
+                // Whether the authentication session is persisted across
+                // multiple requests. When used with cookies, controls
+                // whether the cookie's lifetime is absolute (matching the
+                // lifetime of the authentication ticket) or session-based.
+
+                //IssuedUtc = <DateTimeOffset>,
+                // The time at which the authentication ticket was issued.
+
+                //RedirectUri = <string>
+                // The full path or absolute URI to be used as an http
+                // redirect response value.
+            };
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                authProperties);
+
+            _logger.LogInformation("User {Email} logged in at {Time}.",
+                user.Email, DateTime.UtcNow);
+
+            return Ok("Success!");
+        }
+
+
+
+        [HttpGet("unauthorized")]
+        public IActionResult FailedAuthorization()
+        {
+            return Unauthorized();
+        }
+
+
+        private User MockAuthenticateUser(LoginDto loginDto)
+        {
+            return new User
+            {
+                UserId = 1,
+                Email = loginDto.Email,
+                Password = loginDto.Password,
+            };
+        }
+    }
+}
---
+++ API.SingleSignOnCookie/Models/LoginDto.cs
@@ -0,0 +1,9 @@
+﻿namespace API.SingleSignOnCookie.Controllers.Models
+{
+    public class LoginDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
