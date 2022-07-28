f8327660637197eaf92a4b43c22fb6041620db86
Nick Gaudreau <nickgaudreau@outlook.com>
2022-07-27 21:05:27 -0400

    added Cookie Data Protection Shared key with Redis storage run on docker

--- API.SingleSignOnCookie/Program.cs
+++ API.SingleSignOnCookie/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.DataProtection;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// DataProtection => store to redis for this project... but real world app use Azure (unless the server where redis live is super protected)
+builder.Services.AddDataProtection()
+    .PersistKeysToStackExchangeRedis(ConnectionMultiplexer.Connect("127.0.0.1:5003")) // localhost + docker port given to redis [docker run --name sso-redis -p 5003:6379 -d redis]
+    .SetApplicationName("aUniqueNameForTheCookieNetworkOfApps"); // this must be the same for all using the identity SSO cookie
 
 // Auth
 builder.Services.AddAuthorization();
